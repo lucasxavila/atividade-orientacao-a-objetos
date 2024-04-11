f8a8cddefa7d3da43e154a9ea1a40e87c4b6479f
lucasxavila <164033498+lucasxavila@users.noreply.github.com>
2024-04-11 12:26:20 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Pessoa pessoa1 = new Pessoa("João", 20);
+        Pessoa pessoa2 = new Pessoa("Maria", 30);
+
+        System.out.println(pessoa1.falar());
+        System.out.println(pessoa2.falar());
+    }
+}
